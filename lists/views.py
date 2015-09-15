b1d8c9f1c829dec986e0bdaaa3effc31db5fa42c
ARDIAN - 1206208031 <ardian21@bulan.cs.ui.ac.id>
2015-09-15 19:05:58 +0700

    Refactor home page view to use template

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 #from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html> <head><title>To-Do lists</title></head> <body><h2>Ardian - 1206208031</h2><h3>Hello World!</h3></body> </html>')
+    return render(request, 'home.html')   
+# return HttpResponse('<html> <head><title>To-Do lists</title></head> <body><h2>Ardian - 1206208031</h2><h3>Hello World!</h3></body> </html>')
